5cd7fbb4316cfeb4268595dba581150ed7927a4c
Arnav Singh <36072753+itsarnavsingh@users.noreply.github.com>
2021-06-08 18:50:14 +0530

    Create tensorflow_CNN.py

---
+++ tensorflow_CNN.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+
+
+model = models.Sequential()
+
+# For conv2d first parameter is the number of filters or kernels and second parameter is the dimension of filters or kerners
+# second parameter takes a tuple with two integers
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+# we are increasing the number of filters
+
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10,
+                    validation_data=(test_images, test_labels))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+
+
+#By setting verbose 0, 1 or 2 you just say how do you want to 'see' the training progress for each epoch.
+
+#verbose=0 will show you nothing (silent)
+
+#verbose=1 will show you an animated progress bar like this:
+
+#progres_bar
+
+#verbose=2 will just mention the number of epoch like this:
+
+print(test_acc)
